9867120cc7357b07121e5f1063f389157b0a7544
M863-ik <ikubormercy373@gmail.com>
2026-07-20 13:24:24 +0100

    Created member, department and event models

---
+++ models/Department.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const departmentSchema = new mongoose.Schema({
+departmentName: {
+      type: String,
+      required: true,
+      unique: true
+},
+departmentDescription: {
+      type: String,
+      required: true
+},
+departmentLeader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Member"
+}},
+{
+    timestamps: true
+});
+
+const Department = mongoose.model("Department", departmentSchema);
+
+export default Department;
---
+++ models/Event.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+eventName: {
+      type: String,
+      required: true
+},
+eventDescription: {
+      type: String,
+      required: true
+},
+eventDate: {
+      type: Date,
+      required: true
+},
+eventLocation: {
+      type: String,
+      required: true     
+},
+eventFlyers: {
+      type: String
+},
+eventStatus: {
+      type: String,
+      enum: ["Upcoming", "Ongoing", "Completed", "Cancelled", "Postponed"],
+      default: "Upcoming"
+}},
+{
+    timestamps: true
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
